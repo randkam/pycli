3b738833789b5928ff1f91294e3b00d36e456d1b
randkam <randkaldyne005@gmail.com>
2023-12-27 15:02:44 -0500

    addeed arguments for all subparsers

--- PdfTools.py
+++ PdfTools.py
@@ -52,29 +52,7 @@ def mergePDF(input_File, output_File, pageToAdd):
       output_Pdf.write(f)
 
 
-toDo = input("What would you like to do with PDF\n1. remove a page(s)\n2. merge two PDF's\n3.select pages from PDF\n4.swap pages\n")
-if toDo == '1':
-    input_File = input("Enter the name of the PDF file you want to remove a page from: ")
-    pToRemove = int(input("Enter page to be removed: "))
-    removePage(input_File, "output.pdf", pToRemove)
-elif toDo == '2':
-    firstFile = input("Enter name of  first PDF file: ")
-    secondFile = input("Enter name of second PDF file: ")
-    mergePDF(firstFile, "output.pdf", secondFile)
-elif toDo == '3':
-  input_File = input("Enter the name of the PDF file you want to remove a page from: ")
-  keepGoing = True
-  pList = []
-  while keepGoing:
-    pAdd = input("enter page to be kept, enter d when done: ")
-    if pAdd == 'd':
-      keepGoing = False
-    else:
-      pList.append(int(pAdd))
-  keepPage(input_File, "output.pdf", pList)
-elif toDo == '4':
-   input_File = input("enter file which you like to reverse order(PDF must be length of 2):")
-   swap(input_File,"output.PDF")
+
 
 # Some Changes
    
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import PdfTools
 
 '''
 pycli:
@@ -13,8 +14,7 @@ pycli:
    - contains
 '''
 
-def merge_pdfs(args):
-    print(args)
+
 
 # create the top-level parser
 parser = argparse.ArgumentParser(prog='pyCLI')
@@ -32,19 +32,26 @@ pdf_mergeparser.add_argument('-o', '--output', help='Output file', required=True
 pdf_mergeparser.set_defaults(func=merge_pdfs)
 
 pdf_pageswapparser = pdfsubparsers.add_parser('pageswap', help='swap 2 pages')
+pdf_pageswapparser.add_argument('-i','--input', help= 'Input file', required=True )
+pdf_pageswapparser.add_argument('-o', '--output', help='Output file', required=True)
+pdf_pageswapparser.set_defaults(func=swap)
 
 pdf_pagedeleteparser = pdfsubparsers.add_parser('pagedelete', help='delete pages from pdf')
+pdf_pagedeleteparser.add_argument('-i', '--input', help='Input files', required=True,)
+pdf_pagedeleteparser.add_argument('-p', '--page', help='Page to delete', required=True, nargs='+')
+pdf_pagedeleteparser.add_argument('-o', '--output', help='Output file', required=True)
+pdf_pagedeleteparser.set_defaults(func=removePage)
 
 pdf_pageretainparser = pdfsubparsers.add_parser('pageretain', help='retain pages from pdf')
+pdf_pageretainparser.add_argument('-i', '--input', help='Input files', required=True,)
+pdf_pageretainparser.add_argument('-p', '--page', help='Page to keep', required=True, nargs='+')
+pdf_pageretainparser.add_argument('-o', '--output', help='Output file', required=True)
+pdf_pageretainparser.set_defaults(func=keepPage)
 
 # create the parser for the "b" command
 parser_b = subparsers1.add_parser('files', help='b help')
 
-args = parser.parse_args(['pdf', 'merge', '-i', 'a.pdf', 'b.pdf', 'g.pdf', '-o', 'c.pdf'])
+args = parser.parse_args()
 args.func(args)
-# args = parser.parse_args()
-
-# print(args)
 
 
-#https://docs.python.org/3/library/argparse.html#action
