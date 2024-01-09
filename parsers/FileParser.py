60e988b7645707c333b55712399db3c3bc9cc90e
randkam <randkaldyne005@gmail.com>
2024-01-08 22:12:25 -0500

    fileParser updated

--- parsers/FileParser.py
+++ parsers/FileParser.py
@@ -8,7 +8,7 @@ def createPdfParser(fileParser):
     deleteFilesContainingStringParser.add_argument('-w','--word', help= 'delete files including this word/character in the filename', required= True)
     deleteFilesContainingStringParser.set_defaults(func= FileTools.deleteFilesContainingString)
 
-    mergeFilesOfSameTypeParser = fileSubParser.add_parser('mergetxt', help = 'merge txt files in folder')
+    mergeFilesOfSameTypeParser = fileSubParser.add_parser('merge', help = 'merge files in folder')
     mergeFilesOfSameTypeParser.add_argument('-f','--folder',help='input folder', required=True)
     mergeFilesOfSameTypeParser.add_argument('-o','--output',help='output file', required=True)
     mergeFilesOfSameTypeParser.set_defaults(func = FileTools.mergeFilesOfSameType)
